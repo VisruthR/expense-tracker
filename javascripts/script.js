d1a3c627c437ec2336ae09d5844cfa7a8cec7bc3
ThejasKrishnaL <thejaskrishnanlijesh@gmail.com>
2026-03-17 22:37:57 +0530

    feat: added expense tracker layout in the html page and set a basic design and store the inserted value to a value

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,20 +1,13 @@
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
-alert("This website is under developemt !!")
+function getData(){
+    //taking values
+    let item = document.querySelector('.item-input');
+    let category = document.querySelector('#category-selector')
+    let amount = document.querySelector('.amount-input')
+
+    // storing values
+    let itemValue = item.value;
+    let categoryValue = category.value;
+    let amountValue = amount.value;
+
+}
+ getData()
