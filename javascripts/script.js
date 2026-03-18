e03d55d02dcdb63f4e4e526ec9d926d2eebbe168
VisruthR <restro841@gmail.com>
2026-03-18 14:39:33 +0530

    feat: inserted data is displayed in the your-index table

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,12 +1,24 @@
-function getData(){
-    //taking values
-    let item = document.querySelector('.item-input');
-    let category = document.querySelector('#category-selector')
-    let amount = document.querySelector('.amount-input')
+function getData() {
+  //taking values
+  let item = document.querySelector(".item-input");
+  let category = document.querySelector("#category-selector");
+  let amount = document.querySelector(".amount-input");
 
-    // storing values
-    let itemValue = item.value;
-    let categoryValue = category.value;
-    let amountValue = amount.value;
+  // storing values
+  let itemValue = item.value;
+  let categoryValue = category.value;
+  let amountValue = amount.value;
 
+  if (itemValue == null || categoryValue === "default" || amountValue === "$") {
+    alert("Please fill the fields below to enter");
+    return;
+  }
+
+  const tableBody = document.querySelector("#viewInsertedData");
+
+  tableBody.innerHTML += `<tr>
+    <td>${itemValue}</td>
+    <td>${categoryValue}</td>
+    <td>$${amountValue}</td>
+    </tr>`;
 }
