8a04912288a4cef83d7ee45610c3ecf4296ea55a
VisruthR <restro841@gmail.com>
2026-03-18 17:41:34 +0530

    feat: inserted data is cleared after pressing button

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,5 @@
+const date = new Date().toLocaleDateString("en-GB");
+
 function getData() {
   //taking values
   let item = document.querySelector(".item-input");
@@ -9,16 +11,25 @@ function getData() {
   let categoryValue = category.value;
   let amountValue = amount.value;
 
+  //checking if value empty
   if (itemValue == null || categoryValue === "default" || amountValue === "$") {
     alert("Please fill the fields below to enter");
     return;
   }
 
+  //getting output table
   const tableBody = document.querySelector("#viewInsertedData");
 
+  //updating output table
   tableBody.innerHTML += `<tr>
     <td>${itemValue}</td>
     <td>${categoryValue}</td>
     <td>$${amountValue}</td>
+    <td>${date}</td>
     </tr>`;
+
+  //clearing the input box
+  item.value = "";
+  category.value = "default";
+  amount.value = "";
 }
