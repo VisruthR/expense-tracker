f182d32aac233580f9b19cac944b7cde48663480
VisruthR <restro841@gmail.com>
2026-03-20 22:32:05 +0530

    feat: Working output table

---
+++ javascripts/script.js
@@ -0,0 +1,44 @@
+let total = 0
+const totalDisplay = document.querySelector("#total-value")
+
+function getData() {
+  const itemInput = document.querySelector(".item-input");
+  const categorySelector = document.querySelector("#category-selector");
+  const amountInput = document.querySelector(".amount-input");
+
+  const itemValue = itemInput.value.trim();
+  const amountValue = parseFloat(amountInput.value);
+
+  if (!itemValue || isNaN(amountValue) || amountValue <= 0) {
+    alert("Please provide a valid item name and amount.");
+    return;
+  }
+
+  // Update Total if Paid
+
+  total += amountValue;
+  totalDisplay.textContent = "₹" + total.toFixed(2);
+
+  // Add Row to Table
+  const tableBody = document.querySelector(".output-table");
+  const newRow = `
+        <tr>
+            <td>${itemValue}</td>
+            <td><span class="category-badge">${categorySelector.value}</span></td>
+            <td class="amount-text">₹${amountValue.toLocaleString("en-IN")}</td>
+            <td>${new Date().toLocaleDateString("en-GB")}</td>
+            <td>
+                <button class="table-edit-btn">EDIT</button>
+                <button class="table-delete-btn">DLT</button>
+            </td>
+        </tr>
+    `;
+  tableBody.innerHTML += newRow;
+
+  // Reset
+  itemInput.value = "";
+  amountInput.value = "";
+
+	document.querySelector(".empty-state").style.display = "none"
+
+}
